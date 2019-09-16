2aed465ce5734dcc6472a60c57c92ac7de1fa1be
Giuseppe <giuseppegiovanni.amoruso@gmail.com>
2019-09-16 15:53:12 +0200

    Passed another prop on seach component

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const SearchBox = ({toInput}) => (
+const SearchBox = ({searchValue ,toInput}) => (
         <div>
             <input
             className = 'bg-lightest-blue bn pa2'
             type = 'search'
              placeholder="Search robot..."
-             onChange = {toInput} />
+             onCnhange = {toInput} />
         </div>
     );
 
