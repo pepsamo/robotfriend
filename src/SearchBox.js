78271bf68f474c21896b2bd90f152579ed6f4af4
Giuseppe <giuseppegiovanni.amoruso@gmail.com>
2019-09-16 14:05:17 +0200

    Add style into search component

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const SearchBox = () => {
-    return(
+const SearchBox = () => (
         <div>
-            <input placeholder="Search robot..." />
+            <input
+            className = 'bg-lightest-blue bn pa2'
+             placeholder="Search robot..." />
         </div>
-    )
-}
+    );
 
 export default SearchBox;
