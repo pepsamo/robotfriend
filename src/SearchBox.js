302c36138c312a870ae51d164e392588d944ae00
Giuseppe <giuseppegiovanni.amoruso@gmail.com>
2019-09-16 15:49:47 +0200

    Set event to search component

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const SearchBox = () => (
+const SearchBox = ({toInput}) => (
         <div>
             <input
             className = 'bg-lightest-blue bn pa2'
             type = 'search'
-             placeholder="Search robot..." />
+             placeholder="Search robot..."
+             onChange = {toInput} />
         </div>
     );
 
