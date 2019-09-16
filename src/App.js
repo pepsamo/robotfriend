f0d8b5741609ffc966ddc09a69e738a927923f0e
Giuseppe <giuseppegiovanni.amoruso@gmail.com>
2019-09-16 14:00:06 +0200

    Add search component into app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import {robots} from './robots';
+import SearchBox from './SearchBox';
 
 class App extends Component {
     render() {
         return(
             <div>
                 <h1>Robot Friends</h1>
+                <SearchBox />
                 <CardList robots = {robots} />
             </div>
         )
