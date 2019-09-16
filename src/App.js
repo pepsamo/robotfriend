f4d64c9ff28948e040553ef59b3842cbc24dd52d
Giuseppe <giuseppegiovanni.amoruso@gmail.com>
2019-09-16 13:45:12 +0200

    Add App component

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        return(
+            <CardList robots = {robots} />
+        )
+    }
+}
+
+export default App;
