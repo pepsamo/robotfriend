cf9d483f6fd720fc162a8485fa5a252d3ed05ef8
Giuseppe <giuseppegiovanni.amoruso@gmail.com>
2019-09-16 13:57:23 +0200

    Create default search

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 const SearchBox = () => {
     return(
-        
+        <div>
+            <input placeholder="Search robot..." />
+        </div>
     )
 }
+
+export default SearchBox;
