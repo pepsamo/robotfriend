1141fe97e4d7d2fabef1df9d838cc730525f3ab6
Giuseppe <giuseppegiovanni.amoruso@gmail.com>
2019-09-20 01:44:00 +0200

    Delete robots.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     constructor() {
         super()
         this.state= {
-            robots: robots,
+            robots: [],
             searchValue: ""
         }
 
